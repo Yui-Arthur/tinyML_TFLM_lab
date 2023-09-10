52b8c72f295cbacd72c469dc8bada820cf481b12
Yui-Arthur <arthur80633@gmail.com>
2023-09-10 20:32:14 +0800

    update data

--- export_img.py
+++ export_img.py
@@ -6,20 +6,40 @@ import subprocess
 def parse_opt():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--img', type=str, help='image path' , default="data/images/dogs.jpg")
+    parser.add_argument('--img', type=str, help='image path' , default="data/images/dog.jpg")
 
     opt = parser.parse_args()
     return opt
 
+
 def main(opt):
     
     img = Image.open(opt['img']).resize((32,32))
-    img = np. array(img)
-    bin_img = ''.join(opt['img'].split('.')[:-1]) + ".bin"
-    img.tofile(bin_img)
-    cc_img = ''.join(opt['img'].split('.')[:-1]) + ".cc"
+    img = np.array(img).astype(np.float32)
+    
+    with open("img.cc" , 'w')  as f:
+        f.write("float img_bin[] = {\n")
+        idx = 0
+        for w in img:
+            for h in w:
+                for c in h:
+                    idx+=1
+                    if idx != 32*32*3:
+                        f.write(f"{c}, ")
+                    else:
+                        f.write(f"{c}")
+            f.write("\n")
 
-    subprocess.run(f"xxd -i {bin_img} > {cc_img}" , shell=True , timeout=5)
+        f.write("};\n")
+        f.write("unsigned int img_bin_len = 3072;")
+
+
+
+    # bin_img = ''.join(opt['img'].split('.')[:-1]) + ".bin"
+    # img.tofile(bin_img)
+    # cc_img = ''.join(opt['img'].split('.')[:-1]) + ".cc"
+
+    # subprocess.run(f"xxd -i {bin_img} > {cc_img}" , shell=True , timeout=5)
 
 
 if __name__ == '__main__':
